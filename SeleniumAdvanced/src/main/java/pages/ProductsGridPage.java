14b85845677f42bacccf3da6dbaebd135b72d47a
Mateusz Tadla <mtadla@sii.pl>
2022-06-26 15:17:13 +0200

    prod grid page

--- SeleniumAdvanced/src/main/java/pages/ProductsGridPage.java
+++ SeleniumAdvanced/src/main/java/pages/ProductsGridPage.java
@@ -1,9 +1,23 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class ProductsGridPage extends BasePage{
     public ProductsGridPage(WebDriver driver) {
         super(driver);
+    }
+    @FindBy(css = ".product-title")
+    List<WebElement> productName;
+    public void openProductWithName(String name){
+        for (WebElement productName : productName){
+            if(productName.equals(name)){
+                productName.click();
+                return;
+            }
+        }
     }
 }
